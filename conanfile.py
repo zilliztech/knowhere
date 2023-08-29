0dff9d2b77776339d9decc7d1613f31ea77add66
zh Wang <zihao.wang@zilliz.com>
2023-08-29 15:27:56 +0800

    Remove libunwind in conanfile
    Signed-off-by: zh Wang <zihao.wang@zilliz.com>

--- conanfile.py
+++ conanfile.py
@@ -89,7 +89,6 @@ class KnowhereConan(ConanFile):
         self.requires("zlib/1.2.12")
         self.requires("double-conversion/3.2.1")
         self.requires("xz_utils/5.2.5")
-        self.requires("libunwind/1.5.0")
         self.requires("fmt/9.1.0")
         self.requires("folly/2023.07.12@milvus/dev")
         if self.options.with_ut:
