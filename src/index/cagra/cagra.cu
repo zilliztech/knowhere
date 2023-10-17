46f5b542c5754352c7a08ad856f2b156c368e358
Yusheng.Ma <Yusheng.Ma@zilliz.com>
2023-10-17 07:54:22 +0000

    cagra test
    Signed-off-by: Yusheng.Ma <Yusheng.Ma@zilliz.com>

--- src/index/cagra/cagra.cu
+++ src/index/cagra/cagra.cu
@@ -26,7 +26,7 @@ KNOWHERE_REGISTER_GLOBAL(GPU_RAFT_CAGRA, [](const int32_t& version, const Object
     return Index<IndexNodeThreadPoolWrapper>::Create(std::make_unique<CagraIndexNode>(version, object),
                                                      cuda_concurrent_size);
 });
-KNOWHERE_REGISTER_GLOBAL(GPU_IVF_FLAT, [](const int32_t& version, const Object& object) {
+KNOWHERE_REGISTER_GLOBAL(GPU_CAGRA, [](const int32_t& version, const Object& object) {
     return Index<IndexNodeThreadPoolWrapper>::Create(std::make_unique<CagraIndexNode>(version, object),
                                                      cuda_concurrent_size);
 });
