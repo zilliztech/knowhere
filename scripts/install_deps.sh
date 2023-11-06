292dcd8d20d22013d14d466093743dc8e18833f2
Enwei Jiao <enwei.jiao@zilliz.com>
2023-11-06 09:49:58 -0800

    Upgrade conan to 1.61.0
    Signed-off-by: Enwei Jiao <enwei.jiao@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -44,7 +44,7 @@ if [[ "${MACHINE}" == "Linux" ]]; then
         #DiskANN dependencies
         sudo apt-get install -y libboost-program-options-dev
         sudo apt-get install -y libaio-dev
-        pip3 install conan==1.58.0
+        pip3 install conan==1.61.0
         conan remote add default-conan-local https://milvus01.jfrog.io/artifactory/api/conan/default-conan-local
         # Pre-installation of openblas can save about 15 minutes of openblas building time.
         # But the apt-installed openblas version is 0.2.20, while the latest openblas version is 0.3.19.
@@ -95,14 +95,14 @@ if [[ "${MACHINE}" == "Linux" ]]; then
         wget -c https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.tar.gz && \
         tar -zxvf cmake-3.22.2-linux-x86_64.tar.gz && \
         sudo ln -sf $(pwd)/cmake-3.22.2-linux-x86_64/bin/cmake /usr/bin/cmake
-        pip3 install conan==1.58.0
+        pip3 install conan==1.61.0
         conan remote add default-conan-local https://milvus01.jfrog.io/artifactory/api/conan/default-conan-local
     fi
 fi
 
 if [[ "${MACHINE}" == "Mac"  ]]; then
     brew install libomp llvm ninja openblas
-    pip3 install conan==1.58.0
+    pip3 install conan==1.61.0
     conan remote add default-conan-local https://milvus01.jfrog.io/artifactory/api/conan/default-conan-local
 fi
 
