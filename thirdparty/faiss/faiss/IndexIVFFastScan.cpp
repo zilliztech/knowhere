9c17716504c38290f8482c28450c88ca21cff377
chasingegg <chao.gao@zilliz.com>
2023-12-25 10:50:38 +0800

    Fix scann range search
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- thirdparty/faiss/faiss/IndexIVFFastScan.cpp
+++ thirdparty/faiss/faiss/IndexIVFFastScan.cpp
@@ -1421,9 +1421,10 @@ void IndexIVFFastScan::range_search_implem_12(
                 ij++;
             }
         }
-        std::sort(qcs.begin(), qcs.end(), [](const QC& a, const QC& b) {
-            return a.list_no < b.list_no;
-        });
+        // comment this code, check out https://github.com/zilliztech/knowhere/issues/171
+        // std::sort(qcs.begin(), qcs.end(), [](const QC& a, const QC& b) {
+        //     return a.list_no < b.list_no;
+        // });
     }
     TIC;
 
