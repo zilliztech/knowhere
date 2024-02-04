96b5a3bb5e49e35bcb6acbf76f9bb0714747f711
cqy123456 <qianya.cheng@zilliz.com>
2024-02-04 01:56:23 -0500

    switch knowhere-test branch
    Signed-off-by: cqy123456 <qianya.cheng@zilliz.com>

--- ci/E2E2.groovy
+++ ci/E2E2.groovy
@@ -56,7 +56,7 @@ pipeline {
                             }
                         }
                     }
-                    checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
+                    checkout([$class: 'GitSCM', branches: [[name: '*/2.2']], extensions: [],
                     userRemoteConfigs: [[credentialsId: 'milvus-ci', url: 'https://github.com/milvus-io/knowhere-test.git']]])
                     dir('tests'){
                       unarchive mapping: ["${knowhere_wheel}": "${knowhere_wheel}"]
