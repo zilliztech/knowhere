f63af687033a2392ff92882c8cd19d965f39fcaa
Cai Yudong <yudong.cai@zilliz.com>
2024-03-05 15:14:52 +0800

    [skip ci] Update prepare_gpu_build.sh (#441)
    Signed-off-by: Yudong Cai <yudong.cai@zilliz.com>

--- scripts/prepare_gpu_build.sh
+++ scripts/prepare_gpu_build.sh
@@ -3,5 +3,5 @@
 # This script is to modify CMakeLists.txt to build knowhere for certain GPU serials
 #   GTX 1060 - sm_61
 #   GTX 1660 - sm_75
-sed 's/set(CMAKE_CUDA_ARCHITECTURES \${supported_archs})/set(CMAKE_CUDA_ARCHITECTURES \"61-real;75-real\")/' CMakeLists.txt > tmp
+sed 's/set(CMAKE_CUDA_ARCHITECTURES .*$/set(CMAKE_CUDA_ARCHITECTURES \"61-real;75-real\")/' CMakeLists.txt > tmp
 mv tmp CMakeLists.txt
