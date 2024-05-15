e0c9c41aa22d8f6e6761a0a54460e4573de15bfe
Alexander Guzhva <alexanderguzhva@gmail.com>
2024-05-15 02:23:29 -0400

    integrate #3166 from Faiss baseline (#563)
    Signed-off-by: Alexandr Guzhva <alexanderguzhva@gmail.com>

--- thirdparty/faiss/faiss/FaissHook.cpp
+++ thirdparty/faiss/faiss/FaissHook.cpp
@@ -8,6 +8,7 @@
 #include <faiss/impl/ScalarQuantizerDC.h>
 #include <faiss/impl/ScalarQuantizerDC_avx.h>
 #include <faiss/impl/ScalarQuantizerDC_avx512.h>
+#include <faiss/impl/ScalarQuantizerDC_neon.h>
 namespace faiss {
 
 sq_get_distance_computer_func_ptr sq_get_distance_computer =
@@ -38,6 +39,16 @@ void sq_hook() {
         sq_sel_quantizer = sq_select_quantizer_ref;
         sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_ref;
     }
+#endif
+
+#if defined(__ARM_NEON)
+    static std::mutex hook_mutex;
+    std::lock_guard<std::mutex> lock(hook_mutex);
+
+    /* for IVFSQ */
+    sq_get_distance_computer = sq_get_distance_computer_neon;
+    sq_sel_quantizer = sq_select_quantizer_neon;
+    sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_neon;
 #endif
 }
 
