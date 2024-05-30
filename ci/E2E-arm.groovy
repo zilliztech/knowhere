f92d2e9726d1aed50c641b9d47a0b1f29a9ffee6
xianliang.li <xianliang.li@zilliz.com>
2024-05-30 14:17:12 +0800

    fix ci
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- ci/E2E-arm.groovy
+++ ci/E2E-arm.groovy
@@ -24,10 +24,6 @@ pipeline {
                         def date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
                         def gitShortCommit = sh(returnStdout: true, script: "echo ${env.GIT_COMMIT} | cut -b 1-7 ").trim()
                         version="${env.CHANGE_ID}.${date}.${gitShortCommit}"
-                        sh "apt-get update || true"
-                        sh "apt-get install -y git python3-pip"
-                        sh "apt-get install -y libaio-dev libopenblas-dev libcurl4-openssl-dev libdouble-conversion-dev libevent-dev libgflags-dev"
-                        sh "pip3 install conan==1.61.0"
                         // sh "conan remote add default-conan-local https://milvus01.jfrog.io/artifactory/api/conan/default-conan-local"
                         sh "cmake --version"
                         sh "mkdir build"
@@ -61,9 +57,6 @@ pipeline {
                     userRemoteConfigs: [[credentialsId: 'milvus-ci', url: 'https://github.com/milvus-io/knowhere-test.git']]])
                     dir('tests'){
                       unarchive mapping: ["${knowhere_wheel}": "${knowhere_wheel}"]
-                      sh "apt-get update || true"
-                      sh "apt-get install -y python3-pip"
-                      sh "apt-get install -y libopenblas-dev libaio-dev libdouble-conversion-dev libevent-dev"
                       sh "pip3 install ${knowhere_wheel}"
                       sh "cat requirements.txt | xargs -n 1 pip3 install"
                       sh "pytest -v -m 'L0'"
