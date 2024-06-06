afdda1e3f1f5272cc9b82783a2ad5ccd5b1358fa
Cai Yudong <yudong.cai@zilliz.com>
2024-06-06 11:26:48 +0800

    [skip ci] Add diskann benchmark test
    Signed-off-by: Cai Yudong <yudong.cai@zilliz.com>

--- benchmark/hdf5/benchmark_float_range_bitset.cpp
+++ benchmark/hdf5/benchmark_float_range_bitset.cpp
@@ -21,28 +21,11 @@
 
 const int32_t GPU_DEVICE_ID = 0;
 
-namespace fs = std::filesystem;
-std::string kDir = fs::current_path().string() + "/diskann_test";
-std::string kRawDataPath = kDir + "/raw_data";
-std::string kL2IndexDir = kDir + "/l2_index";
-std::string kIPIndexDir = kDir + "/ip_index";
-std::string kL2IndexPrefix = kL2IndexDir + "/l2";
-std::string kIPIndexPrefix = kIPIndexDir + "/ip";
-
 constexpr uint32_t kNumRows = 10000;
 constexpr uint32_t kNumQueries = 100;
 constexpr uint32_t kDim = 128;
 constexpr uint32_t kK = 10;
 constexpr float kL2KnnRecall = 0.8;
-
-void
-WriteRawDataToDisk(const std::string data_path, const float* raw_data, const uint32_t num, const uint32_t dim) {
-    std::ofstream writer(data_path.c_str(), std::ios::binary);
-    writer.write((char*)&num, sizeof(uint32_t));
-    writer.write((char*)&dim, sizeof(uint32_t));
-    writer.write((char*)raw_data, sizeof(float) * num * dim);
-    writer.close();
-}
 
 class Benchmark_float_range_bitset : public Benchmark_knowhere, public ::testing::Test {
  public:
@@ -108,11 +91,11 @@ class Benchmark_float_range_bitset : public Benchmark_knowhere, public ::testing
         printf("[%.3f s] Test '%s/%s' done\n\n", get_time_diff(), ann_test_name_.c_str(), index_type_.c_str());
     }
 
+#ifdef KNOWHERE_WITH_DISKANN
     void
     test_diskann(const knowhere::Json& cfg) {
         auto conf = cfg;
         auto radius = conf[knowhere::meta::RADIUS].get<float>();
-        conf["index_prefix"] = (metric_type_ == knowhere::metric::L2 ? kL2IndexPrefix : kIPIndexPrefix);
 
         printf("\n[%0.3f s] %s | %s | radius=%.3f\n", get_time_diff(), ann_test_name_.c_str(), index_type_.c_str(),
                radius);
@@ -138,6 +121,7 @@ class Benchmark_float_range_bitset : public Benchmark_knowhere, public ::testing
         printf("================================================================================\n");
         printf("[%.3f s] Test '%s/%s' done\n\n", get_time_diff(), ann_test_name_.c_str(), index_type_.c_str());
     }
+#endif
 
  protected:
     void
@@ -215,6 +199,7 @@ TEST_F(Benchmark_float_range_bitset, TEST_HNSW) {
     test_hnsw(conf);
 }
 
+#ifdef KNOWHERE_WITH_DISKANN
 TEST_F(Benchmark_float_range_bitset, TEST_DISKANN) {
     index_type_ = knowhere::IndexEnum::INDEX_DISKANN;
 
@@ -241,3 +226,4 @@ TEST_F(Benchmark_float_range_bitset, TEST_DISKANN) {
     index_.value().Build(*ds_ptr, conf);
     test_diskann(conf);
 }
+#endif
