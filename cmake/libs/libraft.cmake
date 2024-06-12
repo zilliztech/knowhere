807cd89e2489ada158c15384877e334281c4bf03
presburger <yusheng.ma@zilliz.com>
2024-06-12 19:37:50 +0800

    update raft from 23.12 to 24.04 (#628)
    Signed-off-by: yusheng.ma <yusheng.ma@zilliz.com>

--- cmake/libs/libraft.cmake
+++ cmake/libs/libraft.cmake
@@ -16,8 +16,8 @@
 add_definitions(-DKNOWHERE_WITH_RAFT)
 add_definitions(-DRAFT_EXPLICIT_INSTANTIATE_ONLY)
 set(RAFT_VERSION "${RAPIDS_VERSION}")
-set(RAFT_FORK "wphicks")
-set(RAFT_PINNED_TAG "knowhere-2.4")
+set(RAFT_FORK "milvus-io")
+set(RAFT_PINNED_TAG "branch-24.04")
 
 rapids_find_package(CUDAToolkit REQUIRED BUILD_EXPORT_SET knowhere-exports
                     INSTALL_EXPORT_SET knowhere-exports)
