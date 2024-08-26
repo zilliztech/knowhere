5ef6195397529f2a8fd15a6b978f2e6c04569ce1
Cai Yudong <yudong.cai@zilliz.com>
2024-08-26 14:41:25 +0800

    Remove redundant e2e steps
    Signed-off-by: Cai Yudong <yudong.cai@zilliz.com>

--- ci/E2E-arm.groovy
+++ ci/E2E-arm.groovy
@@ -25,7 +25,6 @@ pipeline {
                         def gitShortCommit = sh(returnStdout: true, script: "echo ${env.GIT_COMMIT} | cut -b 1-7 ").trim()
                         version="${env.CHANGE_ID}.${date}.${gitShortCommit}"
                         sh "apt-get update || true"
-                        sh "apt-get install -y git python3-pip"
                         sh "apt-get install -y libaio-dev libopenblas-dev libcurl4-openssl-dev libdouble-conversion-dev libevent-dev libgflags-dev"
                         sh "pip3 install conan==1.61.0"
                         // sh "conan remote add default-conan-local https://milvus01.jfrog.io/artifactory/api/conan/default-conan-local"
@@ -62,7 +61,6 @@ pipeline {
                     dir('tests'){
                       unarchive mapping: ["${knowhere_wheel}": "${knowhere_wheel}"]
                       sh "apt-get update || true"
-                      sh "apt-get install -y python3-pip"
                       sh "apt-get install -y libopenblas-dev libaio-dev libdouble-conversion-dev libevent-dev"
                       sh "pip3 install ${knowhere_wheel}"
                       sh "cat requirements.txt | xargs -n 1 pip3 install"
