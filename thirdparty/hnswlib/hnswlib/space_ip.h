58b19c7009f7a5078129f2211d9a06ad772c1565
Cai Yudong <yudong.cai@zilliz.com>
2024-09-02 18:09:48 +0800

    Let HNSW throw exception when calculate distance for unsupported DataType
    Signed-off-by: Cai Yudong <yudong.cai@zilliz.com>

--- thirdparty/hnswlib/hnswlib/space_ip.h
+++ thirdparty/hnswlib/hnswlib/space_ip.h
@@ -8,21 +8,14 @@ namespace hnswlib {
 template <typename DataType, typename DistanceType>
 static DistanceType
 InnerProduct(const void* pVect1, const void* pVect2, const void* qty_ptr) {
-    if constexpr (std::is_same_v<DataType, knowhere::fp16>) {
-        return faiss::fp16_vec_inner_product((const knowhere::fp16*)pVect1, (const knowhere::fp16*)pVect2,
-                                             *((size_t*)qty_ptr));
+    if constexpr (std::is_same_v<DataType, knowhere::fp32>) {
+        return faiss::fvec_inner_product((const DataType*)pVect1, (const DataType*)pVect2, *((size_t*)qty_ptr));
+    } else if constexpr (std::is_same_v<DataType, knowhere::fp16>) {
+        return faiss::fp16_vec_inner_product((const DataType*)pVect1, (const DataType*)pVect2, *((size_t*)qty_ptr));
     } else if constexpr (std::is_same_v<DataType, knowhere::bf16>) {
-        return faiss::bf16_vec_inner_product((const knowhere::bf16*)pVect1, (const knowhere::bf16*)pVect2,
-                                             *((size_t*)qty_ptr));
-    } else if constexpr (std::is_same_v<DataType, knowhere::fp32>) {
-        return faiss::fvec_inner_product((const float*)pVect1, (const float*)pVect2, *((size_t*)qty_ptr));
+        return faiss::bf16_vec_inner_product((const DataType*)pVect1, (const DataType*)pVect2, *((size_t*)qty_ptr));
     } else {
-        size_t qty = *((size_t*)qty_ptr);
-        float res = 0;
-        for (unsigned i = 0; i < qty; i++) {
-            res += (DistanceType)((DataType*)pVect1)[i] * (DistanceType)((DataType*)pVect2)[i];
-        }
-        return res;
+        throw std::runtime_error("Unknown Datatype\n");
     }
 }
 
