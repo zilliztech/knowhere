43770735a9f36665c21eb6c950258299962fbdd1
Buqian Zheng <zhengbuqian@gmail.com>
2024-11-11 21:16:21 +0800

    move sparse index Add to build pool (#933)
    Add SparseInvertedIndexNodeCC to allow being thread safe growing index
    
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- include/knowhere/comp/index_param.h
+++ include/knowhere/comp/index_param.h
@@ -60,6 +60,8 @@ constexpr const char* INDEX_FAISS_HNSW_PRQ = "FAISS_HNSW_PRQ";
 
 constexpr const char* INDEX_SPARSE_INVERTED_INDEX = "SPARSE_INVERTED_INDEX";
 constexpr const char* INDEX_SPARSE_WAND = "SPARSE_WAND";
+constexpr const char* INDEX_SPARSE_INVERTED_INDEX_CC = "SPARSE_INVERTED_INDEX_CC";
+constexpr const char* INDEX_SPARSE_WAND_CC = "SPARSE_WAND_CC";
 }  // namespace IndexEnum
 
 namespace ClusterEnum {
--- include/knowhere/expected.h
+++ include/knowhere/expected.h
@@ -46,6 +46,9 @@ enum class Status {
     invalid_cluster_error = 24,
     cluster_inner_error = 25,
     timeout = 26,
+    internal_error = 27,
+    invalid_serialized_index_type = 28,
+    sparse_inner_error = 29,
 };
 
 inline std::string
@@ -95,6 +98,12 @@ Status2String(knowhere::Status status) {
             return "invalid cluster type";
         case knowhere::Status::cluster_inner_error:
             return "cluster inner error";
+        case knowhere::Status::internal_error:
+            return "internal error (something that must not have happened at all)";
+        case knowhere::Status::invalid_serialized_index_type:
+            return "the serialized index type is not recognized";
+        case knowhere::Status::sparse_inner_error:
+            return "sparse index inner error";
         default:
             return "unexpected status";
     }
