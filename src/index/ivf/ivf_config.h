bdb98cb346770c0301662c4bc6de626ee8fa1a9c
foxspy <xianliang.li@zilliz.com>
2024-11-14 18:14:23 +0800

    update knowhere defeault value (#946)
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- src/index/ivf/ivf_config.h
+++ src/index/ivf/ivf_config.h
@@ -24,7 +24,11 @@ class IvfConfig : public BaseConfig {
     CFG_BOOL ensure_topk_full;  // only take affect on temp index(IVF_FLAT_CC) now
     CFG_INT max_empty_result_buckets;
     KNOHWERE_DECLARE_CONFIG(IvfConfig) {
-        KNOWHERE_CONFIG_DECLARE_FIELD(nlist).description("number of inverted lists.").for_train().set_range(1, 65536);
+        KNOWHERE_CONFIG_DECLARE_FIELD(nlist)
+            .description("number of inverted lists.")
+            .set_default(128)
+            .for_train()
+            .set_range(1, 65536);
         KNOWHERE_CONFIG_DECLARE_FIELD(nprobe)
             .set_default(8)
             .description("number of probes at query time.")
