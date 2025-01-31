4e033f69cd84c1bbcbee374f2290b3996848e533
Micka <ide.mickael@gmail.com>
2025-01-31 16:55:10 +0100

    Fix style
    Signed-off-by: Mickael Ide <mide@nvidia.com>

--- src/common/raft/integration/cagra_index.cu
+++ src/common/raft/integration/cagra_index.cu
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 #include "common/raft/integration/raft_knowhere_index.cuh"
 #include "common/raft/proto/raft_index_kind.hpp"
 
