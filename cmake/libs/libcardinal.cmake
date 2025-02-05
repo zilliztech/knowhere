fcd447d02a179395b401da63065600d67e79a94f
sre-ci-robot <56469371+sre-ci-robot@users.noreply.github.com>
2025-02-05 09:59:05 +0800

    Update cardinal commit (#1047)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- cmake/libs/libcardinal.cmake
+++ cmake/libs/libcardinal.cmake
@@ -1,5 +1,5 @@
 # Use short SHA1 as version
-set(CARDINAL_VERSION 9cbfa6e )
+set(CARDINAL_VERSION 2b2c1e7 )
 set(CARDINAL_REPO_URL "https://github.com/zilliztech/cardinal.git")
 
 set(CARDINAL_REPO_DIR "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cardinal")
