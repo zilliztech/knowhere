af2611dcc1dd4eb51e34cb6f63e1e5c89bfd5d51
lyd1992 <105697319+lyd1992@users.noreply.github.com>
2025-04-18 00:10:25 +0800

    knowhere support riscv (#1160)
    Signed-off-by: yudong <yudong@njis.ac.cn>
    Co-authored-by: yudong <yudong@njis.ac.cn>

--- cmake/utils/platform_check.cmake
+++ cmake/utils/platform_check.cmake
@@ -4,11 +4,13 @@ macro(detect_target_arch)
   check_symbol_exists(__aarch64__ "" __AARCH64)
   check_symbol_exists(__x86_64__ "" __X86_64)
   check_symbol_exists(__powerpc64__ "" __PPC64)
+  check_symbol_exists(__riscv "" __RISCV64)
 
   if(NOT __AARCH64
      AND NOT __X86_64
-     AND NOT __PPC64)
-    message(FATAL "knowhere only support amd64, ppc64 and arm64 architecture.")
+     AND NOT __PPC64
+     AND NOT __RISCV64)
+    message(FATAL "knowhere only support amd64, ppc64, riscv64 and arm64 architecture.")
   endif()
 endmacro()
 
