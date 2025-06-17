7f6f354730c1afbea0f485e70713db8100d9a6d5
lyd1992 <liuyudong@iscas.ac.cn>
2025-06-17 02:11:10 +0000

    Temporary commit for RVV optimization
    Signed-off-by: lyd1992 <liuyudong@iscas.ac.cn>

--- src/simd/hook.cc
+++ src/simd/hook.cc
@@ -27,6 +27,10 @@
 #include "distances_neon.h"
 #endif
 
+#if defined(__riscv_vector)
+#include "distances_rvv.h"
+#endif
+
 #if defined(__ARM_FEATURE_SVE)
 #include "distances_sve.h"
 #endif
@@ -531,6 +535,17 @@ fvec_hook(std::string& simd_type) {
 #endif
     }
 #endif
+
+#if defined(__riscv_vector)
+    fvec_inner_product = fvec_inner_product_rvv;
+    simd_type = "RVV";
+    support_pq_fast_scan = true;
+#else
+    fvec_inner_product = fvec_inner_product_ref;
+    simd_type = "GENERIC";
+    support_pq_fast_scan = false;
+#endif
+
 
 // ToDo MG: include VSX intrinsics via distances_vsx once _ref tests succeed
 #if defined(__powerpc64__)
