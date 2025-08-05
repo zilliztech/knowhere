07b822f4461439f6882625da3b5f0ab9f8936d38
hansu2022 <1090263463@qq.com>
2025-08-05 16:27:24 +0800

    This commit adds target_compile_options to knowhere_utils to enable RVV support.
    Signed-off-by: lyd1992 <liuyudong@iscas.ac.cn>
    
    Signed-off-by: hansu2022 <1090263463@qq.com>

--- cmake/libs/libfaiss.cmake
+++ cmake/libs/libfaiss.cmake
@@ -135,6 +135,7 @@ if(__RISCV64)
   add_library(knowhere_utils STATIC ${UTILS_SRC})
   target_link_libraries(knowhere_utils PUBLIC glog::glog)
   target_link_libraries(knowhere_utils PUBLIC xxHash::xxhash)
+  target_compile_options(knowhere_utils PRIVATE $<$<COMPILE_LANGUAGE>:-march=rv64gcv_zvfhmin -mabi=lp64d>)
 endif()
 
 # ToDo: Add distances_vsx.cc for powerpc64 SIMD acceleration
