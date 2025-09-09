e26a86052e542a6da57fa27fb83b3d93a12ebda2
cqy123456 <39671710+cqy123456@users.noreply.github.com>
2025-09-09 17:25:50 +0800

    enhance: Remove the Restriction of dim % bands == 0 in MinHash LSH (#1342)
    Signed-off-by: cqy123456 <qianya.cheng@zilliz.com>

--- src/index/minhash/minhash_util.cc
+++ src/index/minhash/minhash_util.cc
@@ -105,17 +105,18 @@ struct MinHashJaccardComputer : faiss::DistanceComputer {
  * Uses multi-threading for batch processing of queries.
  */
 Status
-minhash_lsh_hit_with_topk1_opt_search(const char* x, const char* y, size_t u8_dim, size_t mh_lsh_band, size_t nx,
-                                      size_t ny, size_t topk, const BitsetView& bitset, float* distances,
-                                      int64_t* labels) {
+minhash_lsh_hit_with_topk1_opt_search(const char* x, const char* y, size_t size_in_bytes, size_t element_size_in_bytes,
+                                      size_t mh_lsh_band, size_t mh_lsh_r, size_t nx, size_t ny, size_t topk,
+                                      const BitsetView& bitset, float* distances, int64_t* labels) {
     std::vector<minhash::KeyType> base_hash_k;
     std::vector<minhash::ValueType> base_hash_v;
     base_hash_k.reserve(ny * mh_lsh_band);
     base_hash_v.reserve(ny * mh_lsh_band);
     {
-        auto base_kv = minhash::GenTransposedHashKV((const char*)y, ny, u8_dim, mh_lsh_band);
+        auto base_kv = minhash::GenTransposedHashKV((const char*)y, ny, size_in_bytes, element_size_in_bytes,
+                                                    mh_lsh_band, mh_lsh_r);
         minhash::SortHashKV(base_kv, ny, mh_lsh_band);
-        for (auto i = 0; i < ny * mh_lsh_band; i++) {
+        for (size_t i = 0; i < ny * mh_lsh_band; i++) {
             base_hash_k.emplace_back(base_kv[i].Key);
             base_hash_v.emplace_back(base_kv[i].Value);
         }
@@ -123,8 +124,9 @@ minhash_lsh_hit_with_topk1_opt_search(const char* x, const char* y, size_t u8_di
     std::vector<minhash::KeyType> query_hash_k;
     query_hash_k.reserve(nx * mh_lsh_band);
     {
-        auto query_kv = minhash::GenHashKV((const char*)x, nx, u8_dim, mh_lsh_band);
-        for (auto i = 0; i < nx * mh_lsh_band; i++) {
+        auto query_kv =
+            minhash::GenHashKV((const char*)x, nx, size_in_bytes, element_size_in_bytes, mh_lsh_band, mh_lsh_r);
+        for (size_t i = 0; i < nx * mh_lsh_band; i++) {
             query_hash_k.emplace_back(query_kv[i].Key);
         }
     }
@@ -144,7 +146,7 @@ minhash_lsh_hit_with_topk1_opt_search(const char* x, const char* y, size_t u8_di
                                       query_end = std::min((size_t)((row + 1) * kQueryBatch), (size_t)nx)]() {
             for (size_t query_id = query_beg; query_id < query_end; query_id++) {
                 auto query_key = query_hash_k.data() + mh_lsh_band * query_id;
-                for (auto i = 0; i < mh_lsh_band; i++) {
+                for (size_t i = 0; i < mh_lsh_band; i++) {
                     auto hit_id = faiss::u64_binary_search_eq(base_hash_k.data() + i * ny, ny, query_key[i]);
                     if (hit_id != -1) {
                         all_res[query_id].push(base_hash_v[hit_id], 1.0f);
@@ -172,9 +174,9 @@ minhash_lsh_hit_with_topk1_opt_search(const char* x, const char* y, size_t u8_di
  * or exact Jaccard distance computation based on configuration.
  */
 Status
-minhash_ny_batch_search(const char* x, const char* y, size_t u8_dim, size_t mh_lsh_band, size_t mh_element_bit_width,
-                        size_t nx, size_t ny, size_t topk, bool mh_search_with_jaccard, const BitsetView& bitset,
-                        float* distances, int64_t* labels) {
+minhash_ny_batch_search(const char* x, const char* y, size_t size_in_bytes, size_t element_size_in_bytes,
+                        size_t mh_lsh_band, size_t mh_lsh_r, size_t nx, size_t ny, size_t topk,
+                        bool mh_search_with_jaccard, const BitsetView& bitset, float* distances, int64_t* labels) {
     auto pool = ThreadPool::GetGlobalSearchThreadPool();
     std::vector<folly::Future<Status>> futs;
     const int batch_size = 128;
@@ -192,15 +194,14 @@ minhash_ny_batch_search(const char* x, const char* y, size_t u8_dim, size_t mh_l
                 auto cur_distances = distances + topk * i;
 
                 if (mh_search_with_jaccard) {
-                    size_t hash_element_size = mh_element_bit_width / 8;  // in bytes
-                    size_t hash_element_length = u8_dim / hash_element_size;
-                    auto cur_query = (const char*)x + u8_dim * i;
-                    MinHashJaccardKNNSearchByNy(cur_query, (const char*)y, hash_element_length, hash_element_size, ny,
-                                                topk, bitset, cur_distances, cur_labels);
+                    size_t mh_length = size_in_bytes / element_size_in_bytes;
+                    auto cur_query = (const char*)x + size_in_bytes * i;
+                    MinHashJaccardKNNSearchByNy(cur_query, (const char*)y, mh_length, element_size_in_bytes, ny, topk,
+                                                bitset, cur_distances, cur_labels);
                 } else {
-                    auto cur_query = (const char*)x + u8_dim * i;
-                    MinHashLSHHitByNy(cur_query, (const char*)y, u8_dim, mh_lsh_band, ny, topk, bitset, cur_distances,
-                                      cur_labels);
+                    auto cur_query = (const char*)x + size_in_bytes * i;
+                    MinHashLSHHitByNy(cur_query, (const char*)y, size_in_bytes, element_size_in_bytes, mh_lsh_band,
+                                      mh_lsh_r, ny, topk, bitset, cur_distances, cur_labels);
                 }
             }
             return Status::success;
@@ -212,10 +213,29 @@ minhash_ny_batch_search(const char* x, const char* y, size_t u8_dim, size_t mh_l
 }
 }  // namespace
 
+// get the best minhash lsh params (b, r), number of bands and size of each bands
+std::pair<size_t, size_t>
+OptimizeMinHashLSHParams(size_t original_dim, size_t band) {
+    if (band <= 0) {
+        size_t b = 1;
+        return {b, original_dim};
+    } else if (band > original_dim) {
+        size_t b = original_dim;
+        return {b, 1};
+    } else {
+        size_t new_dim = (original_dim / band) * band;
+        size_t b = band;
+        size_t r = new_dim / b;
+        return {b, r};
+    }
+}
+
 std::shared_ptr<minhash::KVPair[]>
-GenHashKV(const char* data, size_t rows, size_t data_size, size_t band) {
-    auto res_kv = std::shared_ptr<minhash::KVPair[]>(new minhash::KVPair[band * rows]);
+GenHashKV(const char* data, size_t rows, size_t data_size, size_t data_element_size, size_t band_num,
+          size_t band_size) {
+    auto res_kv = std::shared_ptr<minhash::KVPair[]>(new minhash::KVPair[band_num * rows]);
     auto batch_num = (rows + kBatch - 1) / kBatch;
+    auto truncated_data_size = band_num * band_size * data_element_size;
     auto build_pool = ThreadPool::GetGlobalBuildThreadPool();
     std::vector<folly::Future<folly::Unit>> futures;
     for (size_t i = 0; i < batch_num; i++) {
@@ -225,18 +245,19 @@ GenHashKV(const char* data, size_t rows, size_t data_size, size_t band) {
             for (size_t j = beg_id; j < end_id; j++) {
                 const char* data_j = data + data_size * j;
                 size_t b = 0;
-                for (; b + 4 <= band; b += 4) {
-                    res_kv.get()[j * band + b] = {GetHashKey(data_j, data_size, band, b), minhash::ValueType(j)};
-                    res_kv.get()[j * band + b + 1] = {GetHashKey(data_j, data_size, band, b + 1),
+                for (; b + 4 <= band_num; b += 4) {
+                    res_kv.get()[j * band_num + b] = {GetHashKey(data_j, truncated_data_size, band_num, b),
                                                       minhash::ValueType(j)};
-                    res_kv.get()[j * band + b + 2] = {GetHashKey(data_j, data_size, band, b + 2),
-                                                      minhash::ValueType(j)};
-                    res_kv.get()[j * band + b + 3] = {GetHashKey(data_j, data_size, band, b + 3),
-                                                      minhash::ValueType(j)};
+                    res_kv.get()[j * band_num + b + 1] = {GetHashKey(data_j, truncated_data_size, band_num, b + 1),
+                                                          minhash::ValueType(j)};
+                    res_kv.get()[j * band_num + b + 2] = {GetHashKey(data_j, truncated_data_size, band_num, b + 2),
+                                                          minhash::ValueType(j)};
+                    res_kv.get()[j * band_num + b + 3] = {GetHashKey(data_j, truncated_data_size, band_num, b + 3),
+                                                          minhash::ValueType(j)};
                 }
-                for (; b < band; b++) {
-                    minhash::KVPair kv = {GetHashKey(data_j, data_size, band, b), minhash::ValueType(j)};
-                    res_kv.get()[j * band + b] = kv;
+                for (; b < band_num; b++) {
+                    minhash::KVPair kv = {GetHashKey(data_j, truncated_data_size, band_num, b), minhash::ValueType(j)};
+                    res_kv.get()[j * band_num + b] = kv;
                 }
             }
         }));
@@ -246,10 +267,12 @@ GenHashKV(const char* data, size_t rows, size_t data_size, size_t band) {
 }
 
 std::shared_ptr<minhash::KVPair[]>
-GenTransposedHashKV(const char* data, size_t rows, size_t data_size, size_t band) {
-    auto res_kv = std::shared_ptr<minhash::KVPair[]>(new minhash::KVPair[band * rows]);
+GenTransposedHashKV(const char* data, size_t rows, size_t data_size, size_t data_element_size, size_t band_num,
+                    size_t band_size) {
+    auto res_kv = std::shared_ptr<minhash::KVPair[]>(new minhash::KVPair[band_num * rows]);
     auto batch_num = (rows + kBatch - 1) / kBatch;
     auto build_pool = ThreadPool::GetGlobalBuildThreadPool();
+    auto truncated_data_size = band_num * band_size * data_element_size;
     std::vector<folly::Future<folly::Unit>> futures;
     for (size_t i = 0; i < batch_num; i++) {
         futures.emplace_back(build_pool->push([&, idx = i]() {
@@ -258,17 +281,18 @@ GenTransposedHashKV(const char* data, size_t rows, size_t data_size, size_t band
             for (size_t j = beg_id; j < end_id; j++) {
                 const char* data_j = data + data_size * j;
                 size_t b = 0;
-                for (; b + 4 <= band; b += 4) {
-                    res_kv.get()[b * rows + j] = {GetHashKey(data_j, data_size, band, b), minhash::ValueType(j)};
-                    res_kv.get()[(b + 1) * rows + j] = {GetHashKey(data_j, data_size, band, b + 1),
+                for (; b + 4 <= band_num; b += 4) {
+                    res_kv.get()[b * rows + j] = {GetHashKey(data_j, truncated_data_size, band_num, b),
+                                                  minhash::ValueType(j)};
+                    res_kv.get()[(b + 1) * rows + j] = {GetHashKey(data_j, truncated_data_size, band_num, b + 1),
                                                         minhash::ValueType(j)};
-                    res_kv.get()[(b + 2) * rows + j] = {GetHashKey(data_j, data_size, band, b + 2),
+                    res_kv.get()[(b + 2) * rows + j] = {GetHashKey(data_j, truncated_data_size, band_num, b + 2),
                                                         minhash::ValueType(j)};
-                    res_kv.get()[(b + 3) * rows + j] = {GetHashKey(data_j, data_size, band, b + 3),
+                    res_kv.get()[(b + 3) * rows + j] = {GetHashKey(data_j, truncated_data_size, band_num, b + 3),
                                                         minhash::ValueType(j)};
                 }
-                for (; b < band; b++) {
-                    minhash::KVPair kv = {GetHashKey(data_j, data_size, band, b), minhash::ValueType(j)};
+                for (; b < band_num; b++) {
+                    minhash::KVPair kv = {GetHashKey(data_j, truncated_data_size, band_num, b), minhash::ValueType(j)};
                     res_kv.get()[b * rows + j] = kv;
                 }
             }
@@ -292,12 +316,13 @@ SortHashKV(const std::shared_ptr<KVPair[]> kv_code, size_t rows, size_t band) {
 }
 
 void
-MinHashLSHHitByNy(const char* x, const char* y, size_t dim, size_t mh_lsh_band, size_t ny, size_t topk,
-                  const BitsetView& bitset, float* vals, int64_t* ids) {
+MinHashLSHHitByNy(const char* x, const char* y, size_t size_in_bytes, size_t element_size_in_bytes, size_t mh_lsh_band,
+                  size_t mh_lsh_r, size_t ny, size_t topk, const BitsetView& bitset, float* vals, int64_t* ids) {
+    size_t truncated_size = mh_lsh_band * mh_lsh_r * element_size_in_bytes;
     MinHashLSHResultHandler res(ids, vals, topk);
     for (size_t i = 0; i < ny; i++) {
         if (bitset.empty() || !bitset.test(i)) {
-            res.push(i, faiss::minhash_lsh_hit(x, y + dim * i, dim, mh_lsh_band));
+            res.push(i, faiss::minhash_lsh_hit(x, y + size_in_bytes * i, truncated_size, mh_lsh_band));
             if (res.full()) {
                 break;
             }
@@ -360,14 +385,14 @@ MinHashJaccardKNNSearchByIDs(const char* x, const char* y, const int64_t* sel_id
 }
 
 Status
-MinHashVecSearch(const char* x, const char* y, size_t u8_dim, size_t mh_lsh_band, size_t mh_element_bit_width,
-                 size_t nx, size_t ny, size_t topk, bool mh_search_with_jaccard, const BitsetView& bitset,
-                 float* distances, int64_t* labels) {
+MinHashVecSearch(const char* x, const char* y, size_t size_in_bytes, size_t element_size_in_bytes, size_t mh_lsh_band,
+                 size_t mh_lsh_r, size_t nx, size_t ny, size_t topk, bool mh_search_with_jaccard,
+                 const BitsetView& bitset, float* distances, int64_t* labels) {
     if (topk == 1 && !mh_search_with_jaccard) {  // most common case : topk = 1 and mh_search_with_jaccard == false
-        return minhash_lsh_hit_with_topk1_opt_search(x, y, u8_dim, mh_lsh_band, nx, ny, topk, bitset, distances,
-                                                     labels);
+        return minhash_lsh_hit_with_topk1_opt_search(x, y, size_in_bytes, element_size_in_bytes, mh_lsh_band, mh_lsh_r,
+                                                     nx, ny, topk, bitset, distances, labels);
     } else {
-        return minhash_ny_batch_search(x, y, u8_dim, mh_lsh_band, mh_element_bit_width, nx, ny, topk,
+        return minhash_ny_batch_search(x, y, size_in_bytes, element_size_in_bytes, mh_lsh_band, mh_lsh_r, nx, ny, topk,
                                        mh_search_with_jaccard, bitset, distances, labels);
     }
 }
@@ -387,13 +412,6 @@ MinhashConfigCheck(const size_t dim, const DataFormatEnum data_type, const uint3
     if ((fun_type & invalid_type) != 0) {
         LOG_KNOWHERE_ERROR_ << "Metric MH_JACCARD only support search and train.";
         return Status::not_implemented;
-    }
-    if (fun_type & PARAM_TYPE::TRAIN) {
-        size_t mh_d = cfg->mh_lsh_band.value();
-        if (dim % mh_d != 0) {
-            LOG_KNOWHERE_ERROR_ << "Metric MH_JACCARD not supported for dim % mh_lsh_band != 0.";
-            return Status::not_implemented;
-        }
     }
     return Status::success;
 }
