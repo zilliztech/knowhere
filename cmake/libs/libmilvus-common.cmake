ed15af3f1b5da785523723edd9a712f63251a94d
xianliang.li <xianliang.li@zilliz.com>
2025-09-11 15:03:50 +0800

    update milvus-common interface
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- cmake/libs/libmilvus-common.cmake
+++ cmake/libs/libmilvus-common.cmake
@@ -1,6 +1,6 @@
 
 set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES "")
-set( MILVUS-COMMON-VERSION 2008bac )
+set( MILVUS-COMMON-VERSION aaff302 )
 set( GIT_REPOSITORY  "https://github.com/zilliztech/milvus-common.git")
 
 message(STATUS "milvus-common repo: ${GIT_REPOSITORY}")
