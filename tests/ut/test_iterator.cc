44ee2170ac690815e5f1b621f9d0a9af4e8397b4
chasingegg <chao.gao@zilliz.com>
2025-09-22 14:40:04 +0800

    Bump milvus-common version
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- tests/ut/test_iterator.cc
+++ tests/ut/test_iterator.cc
@@ -103,7 +103,7 @@ TEST_CASE("Test Iterator Mem Index With Float Vector", "[float metrics]") {
     milvus::cachinglayer::Manager::ConfigureTieredStorage(
         {CacheWarmupPolicy::CacheWarmupPolicy_Disable, CacheWarmupPolicy::CacheWarmupPolicy_Disable,
          CacheWarmupPolicy::CacheWarmupPolicy_Disable, CacheWarmupPolicy::CacheWarmupPolicy_Disable},
-        {1024 * mb, 1024 * mb, 1024 * mb, 1024 * mb, 1024 * mb, 1024 * mb}, true, {10, false, 30});
+        {1024 * mb, 1024 * mb, 1024 * mb, 1024 * mb, 1024 * mb, 1024 * mb}, true, true, {10, false, 30});
 
     auto base_gen = [=]() {
         knowhere::Json json;
