8e73823fda07f19149164b27357795a3cdf1da26
jamesgao-jpg <james.gao@zilliz.com>
2026-04-03 10:34:07 +0000

    cleanup: remove stale RAFT mdspan workaround (now on cuVS 26.02)
    Signed-off-by: jamesgao-jpg <james.gao@zilliz.com>

--- cmake/utils/raft_mdspan_fix.h
+++
@@ -1,9 +0,0 @@
-// Workaround for RAFT's vendored mdspan (aligned_accessor.hpp) which uses
-// std::has_single_bit (<bit>) and std::assume_aligned (<memory>) under C++20
-// without including the required headers. Force-including this header ensures
-// the declarations are available before any RAFT header is processed.
-// Remove after upgrading to RAFT/cuVS 26.02+ (vendored mdspan deleted).
-#if __cplusplus >= 202002L
-#include <bit>
-#include <memory>
-#endif
