065ef6d565e14ad44702c4bef363bb0a73853e2d
Gao <chao.gao@zilliz.com>
2026-07-24 17:16:41 +0800

    optimize bm25 scoring in WAND/BLOCK_WAND/BLOCK_MAXSCORE (#1736)
    * optimize bm25 scoring in WAND/BLOCK_WAND/BLOCK_MAXSCORE
    
    Signed-off-by: chasingegg <chao.gao@zilliz.com>
    
    * update
    
    Signed-off-by: chasingegg <chao.gao@zilliz.com>
    
    ---------
    
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- src/index/sparse/searcher/block_max_wand.h
+++ src/index/sparse/searcher/block_max_wand.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <algorithm>
+#include <cassert>
 #include <utility>
 #include <vector>
 
@@ -27,6 +28,7 @@ class BlockMaxWandSearcher : public RankedSearcher {
         float max_score;
         BlockMaxDataCursor block_max_data_cursor;
         float weight;
+        float qval_p1;
 
         [[nodiscard]] uint32_t
         vec_id() const noexcept {
@@ -74,12 +76,26 @@ class BlockMaxWandSearcher : public RankedSearcher {
                                   const uint32_t max_vec_id, const BitsetView& bitset, float dim_max_score_ratio)
         : RankedSearcher(k),
           filter_bounds_(GetFilterBounds(bitset, max_vec_id)),
-          cursors_(make_cursors(index, query, search_scorer, bitset, dim_max_score_ratio, filter_bounds_)),
-          max_vec_id_(filter_bounds_.upper_bound) {
+          bm25_context_(index.get_row_sums(), *search_scorer),
+          cursors_(
+              make_cursors(index, query, search_scorer, bm25_context_, bitset, dim_max_score_ratio, filter_bounds_)),
+          max_vec_id_(filter_bounds_.upper_bound),
+          scorer_type_(search_scorer->config().scorer_type) {
     }
 
     void
     search() override {
+        if (scorer_type_ == IndexScorerType::BM25) {
+            run<IndexScorerType::BM25>();
+        } else {
+            run<IndexScorerType::IP>();
+        }
+    }
+
+ private:
+    template <IndexScorerType ScorerType>
+    void
+    run() {
         std::vector<Cursor*> ordered_cursors;
         ordered_cursors.reserve(cursors_.size());
         for (auto& en : cursors_) {
@@ -136,11 +152,21 @@ class BlockMaxWandSearcher : public RankedSearcher {
                 // check if pivot is a possible match
                 if (pivot_id == ordered_cursors[0]->vec_id()) {
                     float score = 0;
+                    float doc_norm = 0.0F;
+                    if constexpr (ScorerType == IndexScorerType::BM25) {
+                        doc_norm = bm25_context_.doc_norm(pivot_id);
+                    }
                     for (Cursor* en : ordered_cursors) {
                         if (en->vec_id() != pivot_id) {
                             break;
                         }
-                        float part_score = en->score();
+                        float part_score;
+                        if constexpr (ScorerType == IndexScorerType::BM25) {
+                            const float tf = static_cast<float>(en->index_cursor.val());
+                            part_score = bm25_context_.score(en->qval_p1, tf, doc_norm);
+                        } else {
+                            part_score = en->score();
+                        }
                         score += part_score;
                         block_upper_bound -= en->block_max_score() - part_score;
                         if (!topk_.WouldEnter(block_upper_bound)) {
@@ -218,25 +244,26 @@ class BlockMaxWandSearcher : public RankedSearcher {
         }
     }
 
- private:
     static std::vector<Cursor>
     make_cursors(const IndexType& index, const std::vector<std::pair<uint32_t, float>>& query,
-                 const std::shared_ptr<IndexScorer>& index_scorer, const BitsetView& bitset, float dim_max_score_ratio,
-                 const FilterBounds& filter_bounds) {
+                 const std::shared_ptr<IndexScorer>& index_scorer, const BM25ScoringContext& bm25_context,
+                 const BitsetView& bitset, float dim_max_score_ratio, const FilterBounds& filter_bounds) {
         std::vector<Cursor> cursors;
         cursors.reserve(query.size());
         for (const auto& [dim_id, dim_val] : query) {
-            cursors.push_back(Cursor{GetFilteredPostingListCursor(index, dim_id, bitset, filter_bounds),
-                                     index_scorer->dim_scorer(dim_val),
-                                     dim_max_score_ratio * index.get_dim_max_score(dim_id, dim_val),
-                                     index.get_block_max_data_cursor(dim_id), dim_val});
+            cursors.push_back(Cursor{
+                GetFilteredPostingListCursor(index, dim_id, bitset, filter_bounds), index_scorer->dim_scorer(dim_val),
+                dim_max_score_ratio * index.get_dim_max_score(dim_id, dim_val), index.get_block_max_data_cursor(dim_id),
+                dim_val, bm25_context.query_component(dim_val)});
         }
         return cursors;
     }
 
     FilterBounds filter_bounds_;
+    BM25ScoringContext bm25_context_;
     std::vector<Cursor> cursors_;
     uint32_t max_vec_id_;
+    IndexScorerType scorer_type_;
 };
 
 }  // namespace knowhere::sparse::inverted
